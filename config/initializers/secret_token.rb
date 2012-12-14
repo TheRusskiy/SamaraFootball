03fc3fd4bc89091c7b2c7f085a918ad1fff10cd1
Dmitry Ishkov <dimanrussian@gmail.com>
2012-12-14 07:18:39 +0400

    initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamaraFootball::Application.config.secret_token = '62c3959e0274da2fcdca7247d02cc85168495ee1e23a08a5060ff028bd7f568049bbbeae1e56152fddd6542a8c58bbc9533f43f5f5005753144e69f268226af8'
