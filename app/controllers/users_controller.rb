bbb8528e8d7c633fb8cde0bb5600915988f2c3ad
Dmitry Ishkov <dimanrussian@gmail.com>
2013-01-16 02:35:57 +0400

    user edit fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
       user[:password] = params[:user][:password]
       user[:first_name] = params[:user][:first_name]
       user[:last_name] = params[:user][:last_name]
+      user[:position] = params[:user][:position]
       user.save
       if user.errors.any?
         flash[:warning]="Some Error occurred!"
@@ -42,6 +43,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     else
       flash[:warning]="You are not allowed to edit other profiles!"
+      current_user=nil
       redirect_to root_path
     end
   end
@@ -49,23 +51,30 @@ class UsersController < ApplicationController
   def update
     #Is password right?
     #This check is needed in order to prevent malicious calls from outside a browser
-    user = User.authenticate(params[:user][:login],params[:user][:password])
+    user = User.authenticate(params[:login],params[:password])
     if user.nil?
       flash[:warning]="Wrong credentials! Stop messing with my program!"
       redirect_to root_path
     else
     #Check if login available
-      existing_user = User.find_by_login(params[:user][:new_login])
+      existing_user = User.find_by_login(params[:user][:login])
       #Login is unavailable! :-(
       if not ( existing_user.nil? || existing_user.id.to_s==params[:id])
-        flash[:warning]="User with login \"#{params[:user][:new_login]}\" already exists!"
+        flash[:warning]="User with login \"#{params[:user][:login]}\" already exists!"
         redirect_to edit_user_path(user.id)
-      else
+        return
+      end
+      if ( params[:user][:login].length<4 or params[:user][:password].length<6)
+        flash[:warning]="Password or Nickname is too short!"
+        redirect_to edit_user_path(user.id)
+        return
+      end
       #Everything cool! Go create new user
-        user[:login]= params[:user][:new_login]
-        user[:password] = params[:user][:new_password]
+        user[:login]= params[:user][:login]
+        user[:password] = params[:user][:password]
         user[:first_name] = params[:user][:first_name]
         user[:last_name] = params[:user][:last_name]
+        user[:position] = params[:user][:position]
         user.save
         if user.errors.any?
           flash[:warning]="Some Error occurred!"
@@ -74,7 +83,7 @@ class UsersController < ApplicationController
           flash[:notice]="Your profile was updated!"
           redirect_to user_path(user.id)
         end
-      end
+
     end
   end
 
