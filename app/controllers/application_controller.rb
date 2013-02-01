1df442700313b213f206ab58685b619effe629ad
TheRusskiy <dimanrussian@gmail.com>
2013-02-01 15:27:12 +0400

    tiny localization commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
-    I18n.locale="ru"
+    #I18n.locale="ru"
   end
 
   helper_method :current_user
